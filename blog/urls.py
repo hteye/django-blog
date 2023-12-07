cbd6715dd400703633ca8af74c8bfa4a55e25352
hteye <teyehayford87@gmail.com>
2023-12-06 20:27:31 -0800

    My Initial Commit3

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path("", PostListView.as_view(), name="blog_index"),
+    path("posts/", PostListView.as_view(), name="post_list"),
+    path("posts/<int:pk>", PostDetailView.as_view(), name="blog_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
-# from django.http import Http404
-from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
+from django.http import Http404
+from django.shortcuts import render
+from django.http import HttpResponse
 
 
-# Create your views here.
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class PostListView(ListView):
+    template_name = "post_list.html"
+    queryset = Post.objects.exclude(published_at__exact=None).order_by("-published_at")[:5]
+    context_object_name = "posts"
+
+
+class PostDetailView(DetailView):
+    template_name = "post_detail.html"
+    queryset = Post.objects.exclude(published_at__exact=None).order_by("-published_at")
+    context_object_name = "post"
+
+    def get_object(self, queryset=None):
+        # Override get_object to handle Http404 if the post is not found
+        obj = super().get_object(queryset=queryset)
+        if obj.published_at is None:
+            raise Http404("Post not found")
+        return obj
